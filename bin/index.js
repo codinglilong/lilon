7b3d5d466d73425fd09a260e2187a0ccd92c8aed
lilong.chen <791373142@qq.com>
2021-04-25 13:58:52 +0800

    commit

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-
 const lib = require("lilon-lib");
 
-console.log(lib.sum(1,2))
+const argv = require("process").argv;
+const command = argv[2]
+
+lib[command] ? lib[command]() : console.log("请输入命令")
+// console.log(lib.sum(1,2))
 // console.log("Lilon cli test!!!")
